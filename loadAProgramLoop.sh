1b747ebcbc5985a4676dda48b2a73e47cc7aaeec
Moss Canwell <mossman2142@gmail.com>
2016-11-27 09:59:52 +1300

    changed the steam launcher to fix radeon issue
    because the c++ library that steam bundles lags behind the official one the xorg-radeon driver doesn't work. this pach fixes it by forcing the use of the system library.
    https://wirejungle.wordpress.com/2015/01/09/how-to-fix-broken-steam-linux-client-with-radeon-graphics-driver-workaround/

--- loadAProgramLoop.sh
+++ loadAProgramLoop.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 function steamL {
+	export LD_PRELOAD='/usr/$LIB/libstdc++.so.6' #Export so all child processes are affected as well
+	export DISPLAY=:0
+	#export LIBGL_DEBUG=verbose
 	steam
 	if [ $? ]
 	then
